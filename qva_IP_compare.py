e53c2d93f3fb679084711f926d3ec9e495151601
Ehren Andrus <ehrenandrus@gmail.com>
2025-08-18 13:33:48 -0500

    tweaked main logic. empty lines ignored

--- qva_IP_compare.py
+++ qva_IP_compare.py
@@ -44,15 +44,6 @@ def expand_cidr(item, file_num):
             array_file2.append(str(ip))
 
 
-'''def convert_to_cidr(item, file_num):
-    ips = [ip.strip() for ip in item.split("-")]
-    start_ip = ipaddress.IPv4Address(ips[0])
-    end_ip = ipaddress.IPv4Address(ips[1])
-    cidrs = ipaddress.summarize_address_range(start_ip, end_ip)
-
-    for cidr in cidrs:
-        expand_cidr(cidr, file_num)'''
-
 def expand_range(item, file_num):
     ips = [ip.strip() for ip in item.split("-")]
     start = ipaddress.IPv4Address(ips[0])
@@ -75,25 +66,27 @@ def add_single_IP(item, file_num):
 
 #maybe modify with a separate function for printing output, passing in diff_in_arrays
 def sort_and_compare():
+    print(array_file1)
+    print(array_file2)
     set1 = set(array_file1)
     set2 = set(array_file2)
 
     #create .gitignore directory
-    git_ignore_directory = ".gitignore"
+    out = "output"
     try:
-        os.mkdir(git_ignore_directory)
-        print(f"Creating directory {git_ignore_directory} for output files to avoid accidental pushes to repo.")
+        os.mkdir(out)
+        print(f"Creating directory {out} for output files to avoid accidental pushes to repo.")
     except FileExistsError:
-        print(f"{git_ignore_directory} directory already exists. Saving output files there to avoid accidental pushes to repo.")
+        print(f"{out} directory already exists. Saving output files there to avoid accidental pushes to repo.")
 
     #find unique IPs to file1
     diff_in_array1 = list(set1 - set2)
-    with open(f"{git_ignore_directory}/unique_ips_in_file1.txt", "w") as f:
+    with open(f"{out}/unique_ips_in_file1.txt", "w") as f:
         print(f"{', '.join(diff_in_array1)}", file=f)
 
     #find unique IPs to file2
     diff_in_array2 = list(set2 - set1)
-    with open(f"{git_ignore_directory}/unique_ips_in_file2.txt", "w") as f:
+    with open(f"{out}/unique_ips_in_file2.txt", "w") as f:
         print(f"{', '.join(diff_in_array2)}", file=f)
 
     #print unique IPs or print everything matches
@@ -114,35 +107,26 @@ def sort_and_compare():
         print(f"Everything Matches!! Output printed to match.txt.")
 
 def main():
+    files = []
     if len(sys.argv) == 3:
-        file1 = sys.argv[1]
-        file2 = sys.argv[2]
+        files.append(sys.argv[1])
+        files.append(sys.argv[2])
     else:
         print(f"Usage: ./this_script fileToCompare1 fileToCompare2")
         sys.exit(1)
 
     # read files
-    try:
-        with open(file1, "r") as f:
-            for line in f:
-                # parse data to arrays and strip white spaces
-                arr1 = sanitize_data(line)
-                for item in arr1:
-                    parse(item.strip(), 1)
-    except FileNotFoundError:
-        print(f"{file1} does not exist. Please choose existing file.")
-
-    # read files
-    try:
-        with open(file2, "r") as f:
-            for line in f:
-                # parse data to arrays and strip white spaces
-                arr2 = sanitize_data(line)
-
-                for item in arr2:
-                    parse(item.strip(), 2)
-    except FileNotFoundError:
-        print(f"{file2} does not exist. Please choose existing file.")
+    for i in range(len(sys.argv) - 1):
+        try:
+            with open(files[i], "r") as f:
+                for line in f:
+                    if not line.isspace():
+                        # parse data to arrays and strip white spaces
+                        arr = sanitize_data(line)
+                        for item in arr:
+                            parse(item.strip(), i + 1)
+        except FileNotFoundError:
+            print(f"{files[i]} does not exist. Please choose existing file.")
 
     sort_and_compare()
 
